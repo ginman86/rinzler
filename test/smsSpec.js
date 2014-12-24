2a0b8f33897b684bffe31b7c29c9e13030562673
Greg Inman <greg.inman@integritystl.com>
2014-12-24 00:08:14 -0600

    initial commit, w/tests

---
+++ test/smsSpec.js
@@ -0,0 +1,24 @@
+var mocha = require('mocha');
+var chai = require('chai');
+var sinon = require('sinon');
+var assert = require('assert');
+var sms = require('../modules/sms');
+
+var STUB_TEXT_MSG = "casablanca";
+
+describe('The sms module suite', function() {
+  it('should validate an incoming text', function(done) {
+    var spy = sinon.spy(sms, 'validate');
+
+    sms.onDataReceived(STUB_TEXT_MSG);
+
+    assert(spy.called);
+    done();
+  });
+  it('should send a test message.', function(done) {
+    var result = sms.sendTestMessage();
+
+    assert(result)
+    done();
+  });
+});
